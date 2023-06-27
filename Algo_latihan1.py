58f438b0c953ed3151a1b63d70b42ed65ef7f163
aditmusyawaroh89 <127455505+aditmusyawaroh89@users.noreply.github.com>
2023-06-27 15:01:39 +0700

    Create Algo_latihan1.py

---
+++ Algo_latihan1.py
@@ -0,0 +1,35 @@
+# Setting variabel
+nim  = "20122071"
+nama = "nicko aditya firmansyah 
+kelas= "2A"
+prodi= "Informatika"
+
+# Setting variabel nilai
+bhs_indo =76
+bhs_ing  =70
+pd       =90
+mtk      =65
+kal1     =67
+so       =75
+
+# Perhitungan
+rata = (bhs_indo+bhs_ing+pd+mtk+kal1+so)/6
+
+# Cetak hasil
+print("-------------------------------------")
+print("NIM Anda             :",nim)
+print("Nama Lengkap Anda    :",nama)
+print("Kelas                :",kelas)
+print("Program Studi        :",prodi)
+print("-------------------------------------")
+print("     Hasil Kartu Hasil Studi         ")
+print("-------------------------------------")
+print("1. bahasa Indonesia      ",bhs_indo)
+print("2. Bahasa Inggris        ",bhs_ing)
+print("3. Pemrograman Dasar     ",pd)
+print("4. MTK                   ",mtk)
+print("5. Kalkulus 1            ",kal1)
+print("6. Sistem Operasi        ",so)
+print("-------------------------------------")
+print("Rata-rata                ",rata)
+print("------------------------------")
