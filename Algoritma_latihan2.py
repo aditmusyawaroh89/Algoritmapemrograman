9922df180b8aa3fc966caa8d8f5127b5f042dc4f
aditmusyawaroh89 <127455505+aditmusyawaroh89@users.noreply.github.com>
2023-06-27 15:03:35 +0700

    Create Algoritma_latihan2.py

---
+++ Algoritma_latihan2.py
@@ -0,0 +1,43 @@
+# Setting variable
+nim = "20122071"
+nama = "Nicko Aditya Firmansyah"
+kelas = "2A"
+prodi = "Informatika"
+
+# Setting Variable Nilai
+a =10
+b =20
+c =50
+p =10
+l =5
+t =20
+al =30
+
+# Perhitungan
+zigma = ((a+b)*(b*c))/(a+b+c)
+volumebalok = (p*l*t)
+luassegitiga = (1/2*al*t)
+kwadrat = (a+b)**2+(b*c)**2/(a*b)
+
+# Cetak Hasil
+print("-------------------------------------------------------------------")
+print("NIM Anda                  :",nim)
+print("Nama Lengkap Anda         :",nama)
+print("Kelas                     :",kelas)
+print("Program Studi             :",prodi)
+print("-------------------------------------------------------------------")
+print("                      Hasil Perhitungan                            ")
+print("-------------------------------------------------------------------")
+print("1.A                       ",a)
+print("2.B                       ",b)
+print("3.C                       ",c)
+print("4.P                       ",p)
+print("5.L                       ",l)
+print("6.T                       ",t)
+print("7.AL                      ",al)
+print("-------------------------------------------------------------------")
+print("Zigma                     ",zigma)
+print("Volume Balok              ",volumebalok)
+print("Luas Segitiga             ",luassegitiga)
+print("Kwadrat                   ",kwadrat)
+print("-------------------------------------------------------------------")
